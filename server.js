f4b2f909ca003400c3fb06a169cc282c2796999c
Arturo Medina <arturo.medina.mendoza@intel.com>
2024-05-29 17:49:01 -0600

    Added mysql dependency; Showing the port number on console

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
-
+var port = 4000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || port, () => {
+  console.log(`GCS-Services app listening at ${port}`);
+});
