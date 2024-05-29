0ad8edded1015b7633ef0240daec70c494aac2ef
Arturo Medina <arturo.medina.mendoza@intel.com>
2024-05-29 15:55:37 -0600

    Adding app.js scritp

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-        1 file(s) copied.
+const { createServer } = require('node:http');
+const hostname = 'https://GCS-Services.apps1-fm-int.icloud.intel.com';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
