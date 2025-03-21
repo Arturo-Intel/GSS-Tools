11e068147cd6cfa1747428b5e65acdc98c54c5c8
r2 <arturorules@gmail.com>
2025-03-21 16:47:19 -0600

    test

--- routes/caseanalyzer.js
+++ routes/caseanalyzer.js
@@ -65,7 +65,7 @@ async function brain(inputCase) {
         return {
             "SSU-path" : ssuPath,
             "SSU-analysis" : SSUAnalysis, 
-            "case-analysis": caseAnalysis.data
+            "case-analysis": caseAnalysis
         }
         
     } catch (err) {
