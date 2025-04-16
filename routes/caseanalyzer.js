9b19c58ea27173be27941402056beaef28160e3d
r2 <arturorules@gmail.com>
2025-04-16 15:11:33 -0600

    debug

--- routes/caseanalyzer.js
+++ routes/caseanalyzer.js
@@ -122,7 +122,7 @@ async function brain(inputCase) {
             
         caseAnalysis = await invokeModel(token, personaCase, inputCase, "caseAnalysis")
         try {
-            console.log(SSUAnalysis.match(/\{([^}]*)\}/g))
+            console.log(SSUAnalysis)
             SSUAnalysisJSON = JSON.parse(SSUAnalysis.match(/\{([^}]*)\}/g));
             LogEventsAnalysisJSON = JSON.parse(LogEventsAnalysis.match(/\{([^}]*)\}/g))
             DXDiagAnalysisJSON = JSON.parse(DXDiagAnalysis.match(/\{([^}]*)\}/g))
